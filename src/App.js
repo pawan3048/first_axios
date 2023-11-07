1994c4a886ee7cf21f230eec684558bc50a3905b
Pawan Kumar <pawankumhar3048@gmail.com>
2023-11-06 16:08:24 -0800

    axios code push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [Images, setImages] = useState([]);
+
+  var getImages = async () => {
+    try {
+      let resp = await axios.get("https://picsum.photos/v2/list");
+      let data = resp.data;
+      setImages(data);
+    } catch (error) {
+      console.log("Error while Fetching...", error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button onClick={getImages} className="px-5 py-3 bg-success m-4"><b>Get Images</b></button>
+      <hr />
+      <h1 style={{ textDecoration: "underline" }}>Image With API</h1>
+      <div className="p-4" style={{border:'2px solid', width:'1280px', margin: '0 auto'}}>
+        {
+          Images.map((elem, i)=>{
+            return <img key={i} style={{margin:'5px', display: 'inline-block'}} src={elem.download_url} width={290} height={200}/>
+          })
+        }
+      </div>
+    </React.Fragment>
   );
 }
 
